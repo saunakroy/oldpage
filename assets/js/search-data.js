9d375f957facad30d974a0c6307004ac5feb68f0
saunakroy <46112571+saunakroy@users.noreply.github.com>
2025-05-12 05:13:23 +0000

    Deploying to gh-pages from @ saunakroy/saunakroy.github.io@8e1d51eb2d86516b4307982cfe7b3ec74aa4e89f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,17 +4,24 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
   },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
+          title: "Projects",
+          description: "A growing collection of my personal and team projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
+          },
+        },{id: "nav-personal-statement",
+          title: "Personal Statement",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/teaching/";
           },
         },{id: "post-a-post-with-image-galleries",
       
@@ -381,49 +388,24 @@ ninja.data = [{
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
-          description: "with background image",
+          description: "In this project, I use custom CNN architecture and transfer learning models such as ResNet to classify MRI scans of brain tumors. I was able to achieve an F1 score of 0.96 after incorporating data augmentation and regularization techniques to improve the model&#39;s accuracy.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
           title: 'project 2',
-          description: "a project with a background image and giscus comments",
+          description: "This is a research paper I published as part of a team during my internship at UMD’s OUR Summer Research program. In this project, I examined undergraduate AI literacy at UMD through several data analysis/science techniques. I used R/NVivo software to visualize patterns in AI literacy data collected from UMD undergraduates and discovered a positive correlation between students’ technical AI knowledge and perception of AI’s risks to humanity, among other trends and patterns. Our paper has been accepted to and presented at the ACM SIGCSE 2025 conference.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+          description: "This is a project I completed as part of my Introduction to Artificial Intelligence class. Here, I implemented seven different algorithms for solving the Traveling Salesman Problem and visualized their efficiency, runtime, cost, etc., through detailed graphs.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
             },},{id: "projects-project-7",
           title: 'project 7',
-          description: "with background image",
+          description: "This is a project I completed as part of my Introduction to Data Science class. Here, I, along with my team, designed a tutorial for analyzing 49 years of wage data in the US using data analysis/machine learning techniques. We utilized regression, ANOVA, and t-tests to assess wage impacts and predict future trends, achieving R-squared values as high as 0.95.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
